6b73c1a3079b8dd9bf31580f16d527ea24b9153d
Maciej Bielecki <zyla@prati.pl>
2016-01-02 04:04:37 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -21,7 +21,7 @@ instance Num Bit where
   fromInteger 1 = B1
 
 cmd :: Signal (Vec 4 Bit)
-cmd = immediate (0 :> 0 :> 1 :> 1 :> Nil)
+cmd = 0 :> 0 :> 1 :> 1 :> Nil
 
 data Seq' a deriving (Functor)
 type Seq = Seq' ()
