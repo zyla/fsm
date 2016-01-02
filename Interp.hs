96dc251da10eedc6f5e1fec3db07a05ae0aa16fc
Maciej Bielecki <zyla@prati.pl>
2016-01-02 12:48:37 +0100

    Changes so far

---
+++ Interp.hs
@@ -0,0 +1,17 @@
+type PC = Int
+
+data Seq a = Seq { unSeq :: PC -> (a, PC) deriving (Functor)
+
+instance Applicative Seq where
+  pure = return
+  (<*>) = ap
+
+instance Monad Seq where
+  return x = Seq $ \pc -> (x, pc)
+  Seq m >>= k = Seq $ \pc ->
+    let (x, pc') = m pc
+    in unSeq (k x) pc'
+
+
+nextState :: Seq PC
+nextState 
