0d4cb17e653d7d891bef3bf81c8ebffa9b10f650
Maciej Bielecki <zyla@prati.pl>
2016-01-02 16:28:25 +0100

    Changes so far

---
+++ FSM2.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE GADTs #-}
+
+type PC = Int
+
+type RegVal = Int
+data Cond = Not Cond | forall a. Eq (Expr a) (Expr a) deriving (Show)
+data Expr = NC String | Index Expr Expr | Const RegVal | RegVal | Input | Incr Expr | X deriving (Show)
