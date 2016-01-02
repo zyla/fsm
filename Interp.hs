7de0df30c7b09a12cff44d6befd9d812d440b07e
Maciej Bielecki <zyla@prati.pl>
2016-01-02 14:22:24 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -11,56 +11,16 @@ type PC = Int
 type RegVal = Int
 data Cond = Not Cond | RegEqual RegVal deriving (Show)
 data Expr = RegVal | Incr Expr deriving (Show)
-data Transition = Output Expr PC | If Cond Transition (Maybe Transition) deriving (Show)
+data Transition = Output Expr PC | If Cond Transition Transition deriving (Show)
 
 type Machine = M.Map PC Transition
 
 type Seq = State Machine
 
 
-compile :: Seq () -> Transitions
-compile seq = mTransitions $ execState seq (Machine 0 M.empty)
+dac = M.fromList
+  [ 0, 
 
-curState :: Seq PC
-curState = gets mCurrentPC
-
-clock :: Seq ()
-clock = do
-  pc <- gets mCurrentPC
-  goto (pc + 1)
-  modify $ \m -> m { mCurrentPC = mCurrentPC m + 1 }
-
-transition :: Transition -> Seq ()
-transition next = do
-  Machine pc transitions <- get
-  put (Machine pc (M.alter (Just . appendT next) pc transitions))
-
-goto target = transition $ Output RegVal target
-
-appendT :: Transition -> Maybe Transition -> Transition
-appendT next Nothing = next
-appendT next (Just (If cond l r)) = If cond l $ Just $ appendT next r
-appendT _    (Just x) = x
-
-
-prog = endlessly $ do
-  repeat 4 outputCommand
-  repeat 12 outputCommand
-  sync
-
-outputCommand = clock
-outputData = clock
-sync = clock
-
-repeat n act = do
-  loop <- curState
-  act
-  transition $ If (Not $ RegEqual n) (Output (Incr RegVal) loop) Nothing
-
-endlessly act = do
-  loop <- curState
-  act
-  goto loop
 
 render :: Transitions -> String
 render transitions = header ++ concatMap trans (M.toList transitions) ++ footer
