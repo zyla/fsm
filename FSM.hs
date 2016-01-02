73696d3c8a5b78963d0e38db48617d5fc44b710c
Maciej Bielecki <zyla@prati.pl>
2016-01-02 03:58:44 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -50,6 +50,5 @@ dac :: Signal (Vec 12 Bit) -> Seq
 dac input = do
   repeat 4 $ \index -> output (0,1,0,cmd !! index)
   repeat 12 $ \index -> do
-    val <- sample input
-    output (0,1,0,val !! index)
+    output (0,1,0,input !! index)
   output (0,1,1,0)
