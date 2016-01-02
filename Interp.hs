00f3d88c07edde6ad0ab3d75f2743a07aa23682f
Maciej Bielecki <zyla@prati.pl>
2016-01-02 12:55:58 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -6,15 +6,16 @@ import Control.Monad.State
 
 type PC = Int
 
-data Machine = Machine { mCurrentPC :: PC, mTransitions :: M.Map PC PC }
+data Machine = Machine { mNextPC :: PC, mTransitions :: M.Map PC PC }
 
 type Seq = State Machine
 
 
 nextState :: Seq PC
 nextState = do
-  pc <- get
-  put (pc + 1)
+  m <- get
+  let pc = mNextPC m
+  put $ m { mNextPC = pc + 1 }
   return pc
 
 goto :: PC -> Seq ()
