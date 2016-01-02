43aa43aeb5d1c0a62a4df525fe2059a2f61dad28
Maciej Bielecki <zyla@prati.pl>
2016-01-02 12:59:01 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -11,6 +11,9 @@ data Machine = Machine { mNextPC :: PC, mTransitions :: M.Map PC PC }
 type Seq = State Machine
 
 
+compile :: Seq () -> Machine
+compile seq = execState seq (Machine 0 M.empty)
+
 nextState :: Seq PC
 nextState = do
   m <- get
@@ -22,3 +25,6 @@ goto :: PC -> Seq ()
 goto next = do
   Machine pc transitions <- get
   put (Machine pc (M.insert pc next transitions))
+
+
+prog = do
