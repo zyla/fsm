ab68f2bbd41f6a2d9ce78a4b70e37de6c81e90cf
Maciej Bielecki <zyla@prati.pl>
2016-01-02 14:46:06 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -9,9 +9,10 @@ import Control.Monad.State
 type PC = Int
 
 type RegVal = Int
+type Output = (Int, Int, Int, Int)
 data Cond = Not Cond | Eq Expr Expr deriving (Show)
 data Expr = Const RegVal | RegVal | Incr Expr deriving (Show)
-data Transition = Output Expr PC | If Cond Transition Transition deriving (Show)
+data Transition = Next Expr PC | If Cond Transition Transition deriving (Show)
 
 type Machine = M.Map PC Transition
 
@@ -22,14 +23,14 @@ type Seq = State Machine
 stateNames = [ "Init", "Cmd", "Data", "Sync" ]
 
 dac = M.fromList
-  [ 0 .: Output (Const 0) 1
+  [ 0 .: Next (Const 0) 1
   , 1 .: If (Eq RegVal (Const 3))
-           (Output (Const 0) 2) -- (Const 0) comes from 2
-           (Output (Incr RegVal) 1)
+           (Next (Const 0) 2) -- (Const 0) comes from 2
+           (Next (Incr RegVal) 1)
   , 2 .: If (Eq RegVal (Const 11))
-           (Output (Const 0) 3) -- (Const 0) comes from 3)
-           (Output (Incr RegVal) 2)
-  , 3 .: Output (Const 0) 1 -- (Const 0) comes from 1
+           (Next (Const 0) 3) -- (Const 0) comes from 3)
+           (Next (Incr RegVal) 2)
+  , 3 .: Next (Const 0) 1 -- (Const 0) comes from 1
   ]
 
 
@@ -37,7 +38,7 @@ render :: Machine -> String
 render transitions = header ++ concatMap (trans "") (M.toList transitions) ++ footer
   where
     header = "digraph { rankdir=LR; size=\"8,5\"; node [shape=circle]; "
-    trans conds (k, Output rv pc) =
+    trans conds (k, Next rv pc) =
       showPC k ++ "->" ++ showPC pc ++ " [label=\"" ++ conds ++ "R<-" ++ showExpr rv ++ "\"]; "
     trans conds (k, If cond l r) =
       let cstr = conds ++ showCond cond ++ " => "
