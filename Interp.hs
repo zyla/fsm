cf2540d3a05d2efe04d239e3b2c4d697af8ee470
Maciej Bielecki <zyla@prati.pl>
2016-01-02 16:11:08 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -75,6 +75,8 @@ compile prog =
   let concreteStates = zipWith (\index (initial, tr) -> ((index, initial), tr index)) [0..] prog
   in cycleZipWith (\((index, _), tr) (cont, _) -> (index, tr cont)) concreteStates
 
+compile' :: [SD] -> [(PC, Transition)]
+
 cycleZipWith :: (a -> a -> b) -> [a] -> [b]
 cycleZipWith _ [] = []
 cycleZipWith f (hd:xs) = go (hd:xs)
