4d65fcdccc5d927b74d840b82b1e36f44b132f3e
Maciej Bielecki <zyla@prati.pl>
2016-01-02 16:29:59 +0100

    Changes so far

--- FSM2.hs
+++ FSM2.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE ExistentialQuantification #-}
 
 type PC = Int
-
 type RegVal = Int
+
 data Cond = Not Cond | forall a. Eq (Expr a) (Expr a) deriving (Show)
-data Expr = NC String | Index Expr Expr | Const RegVal | RegVal | Input | Incr Expr | X deriving (Show)
+data Expr where
+  Var :: String -> Expr a
+  Index :: Expr [a] -> Expr Int -> Expr a
+  Const :: a -> Expr a
+  Reg ::  | Input | Incr Expr | X deriving (Show)
