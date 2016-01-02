7827b8027368b08cba8abb0b88cec83b9a5db624
Maciej Bielecki <zyla@prati.pl>
2016-01-02 14:39:02 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -39,7 +39,7 @@ render transitions = header ++ concatMap (trans "") (M.toList transitions) ++ fo
     header = "digraph { rankdir=LR; size=\"8,5\"; node [shape=circle]; "
     footer = "}"
     trans conds (k, Output rv pc) =
-      show k ++ "->" ++ show pc ++ " [label=\"" ++ conds ++ "R<-" ++ showExpr rv ++ "\"]; "
+      show k ++ "->" ++ showPC pc ++ " [label=\"" ++ conds ++ "R<-" ++ showExpr rv ++ "\"]; "
     trans conds (k, If cond l r) =
       let cstr = conds ++ showCond cond ++ " => "
       in trans cstr (k, l) ++ trans conds (k, r)
