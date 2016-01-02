db49facf16a4566774b4b1754ce3147b0912281d
Maciej Bielecki <zyla@prati.pl>
2016-01-02 16:32:49 +0100

    Changes so far

--- FSM2.hs
+++ FSM2.hs
@@ -3,14 +3,19 @@
 type PC = Int
 type RegVal = Int
 type Input = Int
+type Output = Int
 
 data Cond = Not Cond | Eq (Expr Int) (Expr Int)
 
 data Expr a where
   Var :: String -> Expr a
+  Tuple :: Expr a -> Expr b -> Expr (a, b)
   Index :: Expr [a] -> Expr Int -> Expr a
   Const :: a -> Expr a
   Reg :: Expr RegVal
   Input :: Expr Input
   Add :: Expr a -> Expr a -> Expr a
   X :: Expr a
+
+
+type Machine = (Expr Output, 
