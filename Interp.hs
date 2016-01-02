77b81836673487868ef07ef89a95a722d5667191
Maciej Bielecki <zyla@prati.pl>
2016-01-02 13:52:38 +0100

    Changes so far

--- Interp.hs
+++ Interp.hs
@@ -10,7 +10,7 @@ type PC = Int
 
 type RegVal = Int
 data Cond = RegEqual RegVal
-data Transition = Output RegVal PC | If Cond Transition Transition
+data Transition = Output Expr PC | If Cond Transition Transition
 
 type Transitions = M.Map PC Transition
 data Machine = Machine { mCurrentPC :: PC, mTransitions :: Transitions } deriving (Show)
