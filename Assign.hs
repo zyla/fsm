24a11ab5e9720df1750e21cebf54d54b964bbf30
Maciej Bielecki <zyla@prati.pl>
2016-01-04 00:18:35 +0100

    Changes so far

--- Assign.hs
+++ Assign.hs
@@ -14,3 +14,5 @@ data Proc = If BExpr Proc | Proc :>> Proc | Assign Assign
 ppProc (If cond proc) = "if " ++ ppBExpr cond ++ " then " ++ ppProc proc ++ " end if"
 ppProc (p1 :>> p2) = ppProc p1 ++ "; " ++ ppProc p2
 ppProc (Assign ass) = ppAssign ass
+
+instance Show Proc where show = ppProc
