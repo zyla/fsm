afe9ec46ba6c90eba223ea7e007c86666439856c
Maciej Bielecki <zyla@prati.pl>
2016-01-04 00:20:30 +0100

    Changes so far

--- Assign.hs
+++ Assign.hs
@@ -4,16 +4,12 @@ import Expr
 
 infix 2 :=
 infix 1 :>>
-data Assign = Ident := AExpr
 
-ppAssign (var := val) = var ++ " <= " ++ ppAExpr val
-
-instance Show Assign where show = ppAssign
-
-data Proc = If BExpr Proc | Proc :>> Proc | Assign Assign
+data Proc = If BExpr Proc | Proc :>> Proc | Ident := AExpr
 
 ppProc (If cond proc) = "if " ++ ppBExpr cond ++ " then " ++ ppProc proc ++ " end if"
 ppProc (p1 :>> p2) = ppProc p1 ++ "; " ++ ppProc p2
 ppProc (Assign ass) = ppAssign ass
+ppProc (var := val) = var ++ " <= " ++ ppAExpr val
 
 instance Show Proc where show = ppProc
