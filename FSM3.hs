b685301a24cf39803fa77d4a03fa37b7589cda71
Maciej Bielecki <zyla@prati.pl>
2016-01-04 00:05:47 +0100

    Changes so far

--- FSM3.hs
+++ FSM3.hs
@@ -21,7 +21,8 @@ ppAExpr (a `Add` b) = ppAExpr a ++ "+" ++ ppAExpr b
 ppBExpr :: BExpr -> String
 ppBExpr (a `Eq` b) = ppAExpr a ++ "=" ++ ppAExpr b
 
-instance Show (Expr a) where show = ppExpr
+instance Show AExpr where
+  show = ppAExpr
 
 instance Num AExpr where
   fromInteger = Lit . fromInteger
