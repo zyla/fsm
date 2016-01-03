028221a23c094687912ca60cb25557291e2704b3
Maciej Bielecki <zyla@prati.pl>
2016-01-04 00:06:03 +0100

    Changes so far

--- FSM3.hs
+++ FSM3.hs
@@ -4,13 +4,13 @@ import Data.String
 
 type Ident = String
 
-data BExpr = Eq AExpr AExpr
-
 data AExpr =
     Var Ident
   | Lit Int
   | Add AExpr AExpr
   | Index AExpr AExpr
+
+data BExpr = Eq AExpr AExpr
 
 ppAExpr :: AExpr -> String
 ppAExpr (Var x) = x
