b69e83e8388c993e11dd311e498ff2d37073ce7a
Maciej Bielecki <zyla@prati.pl>
2016-01-04 00:20:33 +0100

    Changes so far

--- Assign.hs
+++ Assign.hs
@@ -9,7 +9,6 @@ data Proc = If BExpr Proc | Proc :>> Proc | Ident := AExpr
 
 ppProc (If cond proc) = "if " ++ ppBExpr cond ++ " then " ++ ppProc proc ++ " end if"
 ppProc (p1 :>> p2) = ppProc p1 ++ "; " ++ ppProc p2
-ppProc (Assign ass) = ppAssign ass
 ppProc (var := val) = var ++ " <= " ++ ppAExpr val
 
 instance Show Proc where show = ppProc
