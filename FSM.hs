03aeeae90b8f8916f0c45d5bc8e18293ce4ab310
Maciej Bielecki <zyla@prati.pl>
2016-01-04 10:19:51 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -16,7 +16,8 @@ Seq init1 code1 .>> Seq init2 code2 = Seq
   { seqInit = init1
   , seqCode = \self1 cont2 ->
      let cont1 = init2 :| _PC := self2
-         self2 = _ --self1 + length code1'
+         self2 :: PC
+         self2 = self1 + length code1'
          code1' = code1 self1 cont1
      in code1' ++ code2 self2 cont2
   }
