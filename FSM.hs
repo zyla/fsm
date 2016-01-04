ba3ca1320034a3c2c1b15efe47d201be3991a5f4
Maciej Bielecki <zyla@prati.pl>
2016-01-04 10:15:02 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -8,4 +8,6 @@ type Cont = Stmt
 
 data Seq = Seq { seqInit :: Stmt, seqCode :: PC -> Cont -> [Stmt] }
 
-
+-- sequential composition
+(:>>) :: Seq -> Seq -> Seq
+Seq init1 code1 :>> Seq init2 code2
