af71222955f31881adc6ca8325f5effda1c97c5c
Maciej Bielecki <zyla@prati.pl>
2016-01-04 11:17:15 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -63,6 +63,7 @@ compile name portDecls varDecls (initial_assigns, initial_pc, states) = unlines
     , ""
     , "entity " ++ name ++ " is"
     , "  port ("
+    , "  clk : in std_logic;"
     , unlines portDecls
     , "  );"
     , "end entity;"
