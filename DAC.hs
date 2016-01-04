77a973fd8c71b3e4113cd07a2bed9a9215bda6fa
Maciej Bielecki <zyla@prati.pl>
2016-01-04 10:48:41 +0100

    Changes so far

--- DAC.hs
+++ DAC.hs
@@ -13,8 +13,8 @@ output (ldac_n, rst_n, sync_n, sda) =
 dac = seqs
   [ output (0,0,0,0)
   , loop_forever $ seqs
-    [ loop_from_to 0 3 $ \index -> output ("cmd" :! index)
-    , loop_from_to 0 11 $ \index -> output ("input" :! index)
-    , output _
+    [ loop_from_to "x" 0 3 $ \index -> output (0,1,0,"cmd" :! index)
+    , loop_from_to "x" 0 11 $ \index -> output (0,1,0,"input" :! index)
+    , output (0,1,1,0)
     ]
   ]
