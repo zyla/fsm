4b8e02de554ddf792f02fd3e758d25cf836ad774
Maciej Bielecki <zyla@prati.pl>
2016-01-04 11:18:21 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -68,7 +68,8 @@ compile name portDecls varDecls (initial_assigns, initial_pc, states) = unlines
     , "  );"
     , "end entity;"
     , "architecture " ++ name ++ "_arch is"
-    , "  variable PC : integer range " ++ show initial_pc ++ " to " ++ show (initial_pc + length states - 1) ++ ";"
+    , "  variable PC : integer range " ++ show initial_pc ++ " to " ++ show (initial_pc + length states - 1) ++
+        " := " ++ show initial_pc ++ ";"
     , unlines varDecls
     , "begin"
     , "process"
