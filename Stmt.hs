6f9013220420da66e0235ab8ed620ee82ca102c3
Maciej Bielecki <zyla@prati.pl>
2016-01-04 10:14:03 +0100

    Changes so far

--- Stmt.hs
+++ Stmt.hs
@@ -6,14 +6,14 @@ infix 2 :=
 infix 1 :>>
 
 data Stmt =
-    Stmt :>> Stmt
+    Stmt :| Stmt
   | Ident := AExpr
   | If BExpr Stmt
   | Case AExpr [(AExpr, Stmt)]
 
 (.=>) = (,)
 
-ppStmt (p1 :>> p2) = ppStmt p1 ++ " " ++ ppStmt p2
+ppStmt (p1 :| p2) = ppStmt p1 ++ " " ++ ppStmt p2
 ppStmt (var := val) = var ++ " <= " ++ ppAExpr val ++ ";"
 ppStmt (If cond proc) = "if " ++ ppBExpr cond ++ " then " ++ ppStmt proc ++ " end if;"
 ppStmt (Case expr alternatives) = "case " ++ ppAExpr expr ++ " is " ++ concatMap ppAlt alternatives ++ " end case;"
