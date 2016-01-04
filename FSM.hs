8cd3034167c8776ca92fb4a1d4d05112d9f2d8af
Maciej Bielecki <zyla@prati.pl>
2016-01-04 10:37:45 +0100

    Changes so far

--- FSM.hs
+++ FSM.hs
@@ -24,3 +24,24 @@ Seq init1 code1 .>> Seq init2 code2 = Seq
 
 seqs :: [Seq] -> Seq
 seqs = foldr1 (.>>)
+
+loop_forever :: Seq -> Seq
+loop_forever (Seq initial code) = Seq initial $ \self cont -> code self (initial :| _PC := Lit self)
+
+instantiate :: Seq -> (Stmt, PC, [(PC, Stmt)])
+instantiate (Seq initial code) =
+  let start_pc = 0
+  in (initial, start_pc, zip [start_pc..] $ code start_pc (initial :| _PC := Lit start_pc))
+
+output :: Expr Output -> Seq
+output out = (X, \self cont -> [(out, cont)])
+
+loop_from_to :: Expr RegVal -> Expr RegVal -> (Expr RegVal -> Seq) -> Seq
+loop_from_to from to actF =
+  let (_, act) = actF Reg
+  in (from, \self cont -> act self
+       (If (Reg `Eq` to)
+         cont
+         (Const self `Tuple` Add Reg 1)
+       )
+     )
