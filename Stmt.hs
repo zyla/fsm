8325abf5ded0151e21963a96cc502697baa6eff0
Maciej Bielecki <zyla@prati.pl>
2016-01-04 11:29:55 +0100

    Changes so far

--- Stmt.hs
+++ Stmt.hs
@@ -16,7 +16,7 @@ data Stmt =
 
 ppStmt (Nop :| p) = ppStmt p
 ppStmt (p :| Nop) = ppStmt p
-ppStmt (p1 :| p2) = ppStmt p1 ++ " " ++ ppStmt p2
+ppStmt (p1 :| p2) = ppStmt p1 ++ "\n" ++ ppStmt p2
 ppStmt (var := val) = var ++ " <= " ++ ppAExpr val ++ ";"
 ppStmt (If cond iftrue iffalse) = "if " ++ ppBExpr cond ++ " then " ++ ppStmt iftrue ++ " else " ++ ppStmt iffalse ++ " end if;"
 ppStmt (Case expr alternatives) = "case " ++ ppAExpr expr ++ " is " ++ concatMap ppAlt alternatives ++ " end case;"
